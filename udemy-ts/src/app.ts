dcab35ccd7eb2bf94409db4abcca838ffbce86dd
circlesharp <circlesharp@live.com>
2022-01-25 00:38:24 +0800

    feat(ts): udemy-ts 增加 函数装饰器

--- udemy-ts/src/app.ts
+++ udemy-ts/src/app.ts
@@ -1,7 +1,23 @@
+function autoBind(_: any, __: string, descriptor: PropertyDescriptor) {
+  const newDescriptor: PropertyDescriptor = {
+    configurable: descriptor.configurable,
+    enumerable: descriptor.enumerable,
+    get() {
+      return descriptor.value.bind(this);
+    },
+  }
+
+  return newDescriptor;
+}
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
@@ -12,11 +28,29 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#title')! as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
+
+    this.configure();
     this.attach();
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler);
   }
 
   private attach() {
     this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+
+  @autoBind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+    
   }
 }
 
