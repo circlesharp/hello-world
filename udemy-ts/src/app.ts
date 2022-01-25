7050751cba8a93b8cdc6ce353e25e88d80264fe8
circlesharp <circlesharp@live.com>
2022-01-25 14:18:07 +0800

    feat(ts): udemy-ts 增加校验, 并运用了谓词, 条件类型

--- udemy-ts/src/app.ts
+++ udemy-ts/src/app.ts
@@ -1,3 +1,49 @@
+type Validatable<T extends string | number> = (
+  T extends string
+  ? {
+    minLength?: number;
+    maxLength?: number;
+  }
+  : {
+    min?: number;
+    max?: number;
+  }
+) & {
+  value: T;
+  required?: boolean;
+}
+
+type StrValidatable = Validatable<string>;
+type NumValidatable = Validatable<number>;
+
+function isStringValidatable(validatableInput: StrValidatable | NumValidatable): validatableInput is StrValidatable {
+  return typeof validatableInput.value === 'string';
+}
+
+function validate(validatableInput: StrValidatable | NumValidatable): boolean {
+  let isValid = true;
+  if (validatableInput.required) {
+    isValid = isValid && validatableInput.value.toString().trim().length > 0;
+  }
+  if (isStringValidatable(validatableInput)) {
+    if (validatableInput.minLength != null) {
+      isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if (validatableInput.maxLength != null) {
+      isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+  } else {
+    if (validatableInput.min != null) {
+      isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+    if (validatableInput.max != null) {
+      isValid = isValid && validatableInput.value <= validatableInput.max;
+    }
+  }
+
+  return isValid;
+}
+
 function autoBind(_: any, __: string, descriptor: PropertyDescriptor) {
   const newDescriptor: PropertyDescriptor = {
     configurable: descriptor.configurable,
@@ -31,8 +77,8 @@ class ProjectInput {
     this.element.id = 'user-input';
 
     this.titleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
-    this.descriptionInputElement = this.element.querySelector('#title')! as HTMLInputElement;
-    this.peopleInputElement = this.element.querySelector('#title')! as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector('#description')! as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector('#people')! as HTMLInputElement;
 
     this.configure();
     this.attach();
@@ -49,8 +95,51 @@ class ProjectInput {
   @autoBind
   private submitHandler(event: Event) {
     event.preventDefault();
-    console.log(this.titleInputElement.value);
-    
+    const userInput = this.gatherUserInput();
+    if (Array.isArray(userInput)) {
+      const [title, desc, people] = userInput;
+      console.log(title, desc, people);
+      this.clearInputs();
+    }
+  }
+
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    const titleValidatable: StrValidatable = {
+      value: enteredTitle,
+      required: true,
+    }
+    const descriptionValidatable: StrValidatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5,
+    }
+    const peopleValidatable: NumValidatable = {
+      value: Number(enteredPeople),
+      required: true,
+      min: 1,
+      max: 5,
+    }
+
+    if (
+      validate(titleValidatable) &&
+      validate(descriptionValidatable) &&
+      validate(peopleValidatable)
+    ) {
+      return [enteredTitle, enteredDescription, Number(enteredPeople)];
+    } else {
+      alert('Invalid input');
+      return;
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = '';
+    this.descriptionInputElement.value = '';
+    this.peopleInputElement.value = '';
   }
 }
 
