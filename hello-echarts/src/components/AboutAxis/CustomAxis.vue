8dc98df1e1e1c4a5d01e44ee4c06f4312cc3dd7b
circlesharp <circlesharp@live.com>
2022-07-03 11:36:44 +0800

    feat(chart): 更新 echarts 内容

---
+++ hello-echarts/src/components/AboutAxis/CustomAxis.vue
@@ -0,0 +1,51 @@
+<template>
+  <section>
+    <div ref="chartContainer"></div>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref, watch } from 'vue';
+import * as echarts from 'echarts';
+
+let myChart: any;
+
+const chartContainer = ref();
+
+const option = ref({
+  tooltip: {},
+  xAxis: {
+    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子'],
+  },
+  yAxis: {},
+  series: [
+    {
+      name: '销量',
+      type: 'bar',
+      data: [5, 20, 36, 10, 10, 20],
+    },
+  ],
+});
+
+watch(
+  () => option,
+  (opt) => {
+    myChart.setOption(opt.value);
+  }
+);
+
+onMounted(() => {
+  const chartDom = chartContainer.value as HTMLDivElement;
+  if (!chartDom) return;
+
+  myChart = echarts.init(chartDom, undefined, {
+    width: 600,
+    height: 400,
+  });
+
+  myChart.setOption(option.value);
+});
+</script>
---
+++ hello-echarts/src/views/AboutAxis.vue
@@ -0,0 +1,12 @@
+<template>
+  Axis
+  <CustomAxis />
+  <FullAxis />
+</template>
+
+<script setup lang="ts">
+import FullAxis from '../components/AboutAxis/FullAxis.vue';
+import CustomAxis from '../components/AboutAxis/CustomAxis.vue';
+</script>
+
+<style lang="less" scoped></style>
